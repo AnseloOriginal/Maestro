a00123cf87d013328db063e4a3de592af5d9a88d
Uzebu Ansel <102417781+AnseloOriginal@users.noreply.github.com>
2025-12-17 02:37:22 +0100

    Set up Capacitor Android project with CI configs

---
+++ android/app/src/main/java/com/maestro/aba/MainActivity.java
@@ -0,0 +1,5 @@
+package com.maestro.aba;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maestro.aba',
+  appName: 'Maestro',
+  webDir: 'src'
+};
+
+export default config;
