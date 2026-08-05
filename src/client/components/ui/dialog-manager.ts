2cb3886d935c2275e90809682b748608d4552a7e
Uzebu Ansel <102417781+AnseloOriginal@users.noreply.github.com>
2026-08-06 00:03:30 +0100

    Position the dialog manager properly and handle its states

--- src/client/components/ui/dialog-manager.ts
+++ src/client/components/ui/dialog-manager.ts
@@ -6,21 +6,35 @@ export class DialogManager {
 
   constructor() {
     this.root.addEventListener("click", (evt) => this.onPointerClick(evt))
+    this.root.style.zIndex = 1+""
+    this.root.style.position = "fixed"
+    this.root.style.width = "100vw"
+    this.root.style.height = "100vh"
+    this.active(false)
   }
 
-  newDialog(render: renderFunction) {
+  newDialog(
+    render: renderFunction, 
+    width: number = 200,
+    height: number = 200,
+  ) {
     const dialog = new Dialog(
       this.onPointerClick,
-      this.onDialogClose
+      this.onDialogClose,
+      width,
+      height,
+      this.mainStack.length
     )
     this.root.append(dialog.root)
     this.mainStack.unshift(dialog)
     render(dialog)
     dialog.root.show()
+    this.active(true)
   }
 
   onDialogClose = (dialog: Dialog) => {
     dialog.root.close()
+    dialog.root.remove()
     const index = this.mainStack.findIndex((d) => {
       if (d === dialog) {
         return true
@@ -28,6 +42,7 @@ export class DialogManager {
     })
     if (index === -1) { return }
     console.warn("Dialog:",this.mainStack.splice(index,1))
+    this.active(this.mainStack.length > 0)
   }
 
   onPointerClick = (evt: PointerEvent) => {
@@ -46,6 +61,16 @@ export class DialogManager {
       this.onDialogClose(dialog)
     }
   }
+
+  active(isTrue: boolean) {
+    if (isTrue) {
+      this.root.style.pointerEvents = "auto"
+      this.root.style.backdropFilter = "blur(2px)"
+    } else {
+      this.root.style.pointerEvents = "none"
+      this.root.style.backdropFilter = "none"
+    }
+  }
 }
 
 class Dialog {
@@ -54,9 +79,21 @@ class Dialog {
 
   constructor(
     onClick: (evt: PointerEvent, d: Dialog) => void,
-    onClose: (d: Dialog) => void
+    onClose: (d: Dialog) => void,
+    width: number,
+    height: number,
+    zindex: number
   ) {
     this.onClose = onClose
+    this.root.style.minHeight = height+"px"
+    this.root.style.minWidth = width+"px"
+    this.root.style.zIndex = zindex+""
+    this.root.style.position = "absolute"
+    this.root.style.top = "50%"
+    this.root.style.left = "50%"
+    this.root.style.position = "absolute"
+    this.root.style.transform = "translate(-50%, -50%)"
+
   }
 
   close() {
